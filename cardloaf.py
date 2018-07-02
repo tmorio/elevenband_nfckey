0d4d5cb3a9e8de633aecfb253b0f4762adc00557
Morito <inamori@eleken.jp>
2018-07-02 23:17:03 +0900

    README.md Ver1.2

--- cardloaf.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import nfc
-def connected(tag):
-  print tag
-  print str(tag.identifier).encode('hex').upper()
-clf = nfc.ContactlessFrontend('usb')
-clf.connect(rdwr={'on-connect': connected})
